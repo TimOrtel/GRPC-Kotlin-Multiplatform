31816a4330c749ea8558c348df303c4e307b86d0
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2022-10-24 16:54:24 +0200

    Added support for iOS16

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,4 +4,10 @@ plugins {
 
 repositories {
     gradlePluginPortal()
+    google()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    implementation("com.android.tools.build:gradle:7.0.4")
 }
