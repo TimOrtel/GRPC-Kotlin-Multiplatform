3378a27c38c1933a830c42b91d274a198c8bcb57
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2025-04-09 16:07:14 +0200

    Add eq tests.

--- kmp-grpc-internal-test/src/commonTest/kotlin/io/github/timortel/kotlin_multiplatform_grpc_plugin/test/EqTest.kt
+++ kmp-grpc-internal-test/src/commonTest/kotlin/io/github/timortel/kotlin_multiplatform_grpc_plugin/test/EqTest.kt
@@ -1,5 +1,6 @@
 package io.github.timortel.kotlin_multiplatform_grpc_plugin.test
 
+import io.github.timortel.kmpgrpc.core.message.UnknownField
 import io.github.timortel.kmpgrpc.test.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -33,5 +34,41 @@ class EqTest {
     @Test
     fun messageWithAllTypesEquals() {
         assertEquals(createMessageWithAllTypes(), createMessageWithAllTypes())
+    }
+
+    @Test
+    fun messageWithUnknownFieldsEquals() {
+        val message = Unknownfield.MessageWithUnknownField(
+            a = "test.message",
+            unknownFields = listOf(
+                UnknownField.Varint(2, 13L),
+                UnknownField.Fixed32(3, (-4f).toBits().toUInt()),
+                UnknownField.Fixed64(4, (64.0).toBits().toULong()),
+                UnknownField.LengthDelimited(5, "Test Message".encodeToByteArray())
+            )
+        )
+
+        assertEquals(message, message)
+    }
+
+    @Test
+    fun messageWithDifferentUnknownFieldsDiffer() {
+        val message = Unknownfield.MessageWithUnknownField(
+            a = "test.message",
+            unknownFields = listOf(
+                UnknownField.Varint(2, 13L),
+                UnknownField.Fixed32(3, (-4f).toBits().toUInt())
+            )
+        )
+
+        val message2 = Unknownfield.MessageWithUnknownField(
+            a = "test.message",
+            unknownFields = listOf(
+                UnknownField.Varint(2, 13L),
+                UnknownField.Fixed32(3, (-5f).toBits().toUInt()) // difference
+            )
+        )
+
+        assertNotEquals(message, message2)
     }
 }
