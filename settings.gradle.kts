26448396d87f8fa4de2d7b9698bc23d60aba7ef8
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2025-08-24 14:17:01 +0200

    Release: 1.2.0 (#83)
    * Set version to 1.2.0. Upgrade dependencies.
    
    * Move grpc version to core lib relevant section
    
    * Downgrade grpc jvm.
    
    * Downgrade uiTestManifest

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,15 +7,6 @@ pluginManagement {
         gradlePluginPortal()
         google()
     }
-
-    plugins {
-        kotlin("jvm") version "2.1.20"
-        kotlin("multiplatform") version "2.1.20"
-        id("com.android.library")
-
-        id("com.google.protobuf")
-        id("io.github.timortel.kmpgrpc.plugin") version "1.0.0" apply false
-    }
 }
 
 include("kmp-grpc-core")
